8a4e1e9f02dd5c2ddb7b5dc01e57c7108dbf49ae
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-05-25 21:03:47 +0300

    created project

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,19 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+
+    @Test
+    public void loginPositiveTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm("sotiga2018@gmail.com", "Bilbo12345@");
+    }
+
+
+}
